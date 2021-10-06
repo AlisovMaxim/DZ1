2533e268143ed9bf5b0143a28d05f2a63b730b48
AlisovMaxim <alisov.maxim@gmail.com>
2021-10-06 22:02:49 +0300

    1

---
+++ DZ1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    char c[64];
+    int i, x = 0;
+    scanf("%s", &c);
+    for(i = 0; i < strlen(c); i++){
+        x += ((int)c[i] - 48) * ((int)pow(2, strlen(c) - (i+1)));
+    }
+    printf("%d\n", x);
+    return 0;
+}
