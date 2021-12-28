812c2a2e4fa4d39d1e06fd1b851954f21b1591d2
AlisovMaxim <90920167+AlisovMaxim@users.noreply.github.com>
2021-12-28 20:28:23 +0300

    Update DZ1.c

--- DZ1.c
+++ DZ1.c
@@ -5,9 +5,7 @@ int main(void){
     char c[64];
     int i, x = 0;
     scanf("%s", &c);
-    for(i = 0; i < strlen(c); i++){
-        x += ((int)c[i] - 48) * ((int)pow(2, strlen(c) - (i+1)));
-    }
+    for(i = 0; i < strlen(c); i++) x += ((int)c[i] - 48) * ((int)pow(2, strlen(c) - (i+1)));
     printf("%d\n", x);
     return 0;
 }
